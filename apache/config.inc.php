2c919a5177c675910fbe5eae5cc8c3b06428b18a
Gary Yang <gang.yang@vonage.com>
2020-08-27 18:52:06 -0400

    $cfg['Servers'][$i]['auth_type'] = 'http';

--- apache/config.inc.php
+++ apache/config.inc.php
@@ -117,6 +117,7 @@ for ($i = 1; isset($hosts[$i - 1]); $i++) {
     $cfg['Servers'][$i]['compress'] = false;
     $cfg['Servers'][$i]['AllowNoPassword'] = true;
     $cfg['Servers'][$i]['tracking'] = true;
+    $cfg['Servers'][$i]['auth_type'] = 'http';
 }
 for ($i = 1; isset($sockets[$i - 1]); $i++) {
     $cfg['Servers'][$i]['socket'] = $sockets[$i - 1];
